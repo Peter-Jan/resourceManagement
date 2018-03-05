50bca0bb0ffab281faa7ee18ac1e27f4c4fc038e
Peter-Jan <pjan89@gmail.com>
2018-03-05 00:58:48 -0500

    modified thermal network

--- resourceParam.m
+++ resourceParam.m
@@ -4,30 +4,7 @@
 % -------------------------------------------------------------------
                                         
 %HVAC
-LWall = 0.2;
-LWindow = 0.01;
-M = 1778.3691144576505;
-Mdot = 3600;
-RWall = 1.5214818031734424E-6;
-RWindow = 5.9354226020892692E-7;
-Req = 4.2697563775143509E-7;
-THeater = 50;
-TinIC = 20;
-c = 1005.4;
-cost = 2.5E-8;
-densAir = 1.225;
-htHouse = 4;
-htWindows = 1;
-kWall = 136.79999999999998;
-kWindow = 2808;
-lenHouse = 30;
-numWindows = 6;
-pitRoof = 0.69813170079773179;
-r2d = 57.295779513082323;
-wallArea = 960.89765979732;
-widHouse = 10;
-widWindows = 1;
-windowArea = 6;
+load('thermal.mat')
 
 % Electrical System
 costPerKWH = 0.12;
